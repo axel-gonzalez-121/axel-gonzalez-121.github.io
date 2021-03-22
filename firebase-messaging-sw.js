007ed6527f45dfb387e8297cf1e16f4e7f0552d9
axel-gonzalez-121 <axel.gonzalez@greenmarimba.io>
2021-03-22 11:42:08 -0600

    change with new API

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -3,7 +3,7 @@ importScripts('https://www.gstatic.com/firebasejs/8.2.10/firebase-messaging.js')
 
 if ('serviceWorker' in navigator) {
 	navigator.serviceWorker.register('../firebase-messaging-sw.js')
-	// navigator.serviceWorker.register('/test-pwa/firebase-messaging-sw.js')
+		// navigator.serviceWorker.register('/test-pwa/firebase-messaging-sw.js')
 		.then(function (registration) {
 			console.log('Registration successful, scope is:', registration.scope);
 		}).catch(function (err) {
@@ -17,8 +17,8 @@ firebase.initializeApp({
 	projectId: "push-test-8543c",
 	storageBucket: "push-test-8543c.appspot.com",
 	messagingSenderId: "222177857579",
-	appId: "1:222177857579:web:b71df4c74397f5d79c1a6e",
-	measurementId: "G-7M0X3NYC1N"
+	appId: "1:222177857579:web:2f72c805820fe9fd9c1a6e",
+	measurementId: "G-PB3PKBPK2M"
 });
 
 const messaging = firebase.messaging();
